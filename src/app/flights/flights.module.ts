40fae25dfe7f0736a4e686a449916880e3fe1b56
Rifat2022 <md.rifathossainshaon@gmail.com>
2022-12-20 19:08:36 +0600

    upto running the application is done

---
+++ src/app/flights/flights.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlightsComponent } from './flights.component';
+
+
+
+@NgModule({
+  declarations: [
+    FlightsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [], 
+  providers: []
+})
+export class FlightsModule { }
